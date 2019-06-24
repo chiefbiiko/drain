516df7fa3b948f76b8d4dbf0b30cfa0b4f559b0c
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-06-24 20:21:24 +0200

    major changes

--- mod.ts
+++ mod.ts
@@ -1,40 +1,71 @@
+export interface DrainOptions {
+  limit?: number;
+  maxReadTimeout?: number;
+}
+
 export interface Drainage {
-  promise: Promise<void>;
-  cancel: (err?: Error) => void;
+  readonly promise: Promise<void>;
+  readonly cancel: (err?: Error) => void;
 }
 
 export function drain(
   reader: Deno.Reader,
-  ondata: (chunk: Uint8Array) => any
+  ondata: (chunk: Uint8Array) => any,
+  onclose?: () => any,
+  onerror?: (err: Error) => any,
+  { limit, maxReadTimeout }: DrainOptions = { limit: Infinity }
 ): Drainage {
-  const drainage: Drainage = {} as Drainage;
+  // TODO: validate options and everything else also !null
 
-  drainage.promise = new Promise(
-    async (
-      resolve: () => void,
-      reject: (err: Error) => void
-    ): Promise<void> => {
-      drainage.cancel = (err?: Error): void => (err ? reject(err) : resolve());
+  const cancelation: { requested: boolean; error?: Error } = {
+    requested: false
+  };
 
-      const it: AsyncIterableIterator<Uint8Array> = Deno.toAsyncIterator(
-        reader
-      );
+  const drainage: Drainage = {
+    promise: new Promise(
+      async (
+        resolve: () => void,
+        reject: (err: Error) => void
+      ): Promise<void> => {
+        const it: AsyncIterableIterator<Uint8Array> = Deno.toAsyncIterator(
+          reader
+        );
 
-      let result: { done: boolean; value: Uint8Array };
+        let readStart: number = NaN;
 
-      for (;;) {
-        result = await it.next();
+        let result: { done: boolean; value: Uint8Array };
 
-        if (result.done) {
-          break;
+        for (let i: number = 0; i < limit; ++i) {
+          if (cancelation.requested) {
+            return cancelation.error ? reject(cancelation.error) : resolve();
+          }
+
+if (maxReadTimeout !== Infinity) {
+  readStart = Date.now()
+}
+
+          result = await it.next();
+
+     if (maxReadTimeout !== Infinity && Date.now() - readStart > maxReadTimeout) {
+       return reject(new Error(`maxReadTimeout of ${maxReadTimeout}ms exceeded.`))
+     }
+
+          // TODO: can an ait yield done and value in the same step?
+          if (result.done) {
+            return resolve();
+          }
+
+          ondata(result.value);
         }
 
-        ondata(result.value);
+        resolve();
       }
-
-      resolve();
+    ).then(onclose, onerror),
+    cancel(err?: Error): void {
+      cancelation.requested = true;
+      cancelation.error = err;
     }
-  );
+  };
 
   return drainage;
 }
--- test.ts
+++ test.ts
@@ -1,10 +1,14 @@
 import { test, runIfMain } from "https://deno.land/std/testing/mod.ts";
-import { assertEquals, fail } from "https://deno.land/std/testing/asserts.ts";
+import {
+  assert,
+  assertEquals,
+  fail
+} from "https://deno.land/std/testing/asserts.ts";
 import {
   encode,
   decode
 } from "https://denopkg.com/chiefbiiko/std-encoding/mod.ts";
-import { drain } from "./mod.ts";
+import { Drainage, drain } from "./mod.ts";
 
 test({
   name: "drains readers non-blocking",
@@ -23,20 +27,205 @@ test({
 
     let dest: string = "";
 
-    function ondata(chunk: Uint8Array): void {
-      dest += decode(chunk, "utf8");
-    }
-
-    // After calling drain on a Deno.Reader the ondata handler will
-    // be called with every chunk read from the reader
-    drain(conn, ondata)
-      // drain reutrns a Drainage which has two props: promise and cancel()
-      .promise // onclose
-      .then((): void => assertEquals(dest, "419"))
-      // onerror
-      .catch(fail);
+    drain(
+      conn,
+      function ondata(chunk: Uint8Array): void {
+        dest += decode(chunk, "utf8");
+      },
+      function onclose(): void {
+        assertEquals(dest, "419");
+      },
+      function onerror(err: Error): void {
+        fail(err.message);
+      }
+    );
 
     assertEquals(dest, "");
+  }
+});
+
+test({
+  name: "drainage can be cancelled",
+  fn(): Promise<void> {
+    return new Promise(
+      (resolve: () => void, reject: (err: Error) => void): void => {
+        // a never-ending reader
+        const reader: Deno.Reader = {
+          async read(buf: Uint8Array): Promise<Deno.ReadResult> {
+            buf.fill(99);
+            return { nread: buf.length, eof: false };
+          }
+        };
+
+        let count: number = 0;
+
+        const drainage: Drainage = drain(
+          reader,
+          function ondata(chunk: Uint8Array): void {
+            ++count;
+          },
+          function onclose(): void {
+            assert(count !== 0);
+            resolve();
+          },
+          function onerror(err: Error): void {
+            reject(err);
+          }
+        );
+
+        // canceling the drainage.promise
+        drainage.cancel();
+      }
+    );
+  }
+});
+
+test({
+  name: "cancelation with an error is actually a rejection",
+  fn(): Promise<void> {
+    return new Promise(
+      (resolve: () => void, reject: (err: Error) => void): void => {
+        // a never-ending reader
+        const reader: Deno.Reader = {
+          async read(buf: Uint8Array): Promise<Deno.ReadResult> {
+            buf.fill(99);
+            return { nread: buf.length, eof: false };
+          }
+        };
+
+        let count: number = 0;
+
+        const drainage: Drainage = drain(
+          reader,
+          function ondata(chunk: Uint8Array): void {
+            ++count;
+          },
+          function onclose(): void {
+            reject(null);
+          },
+          function onerror(err: Error): void {
+            assertEquals(err.message, "fraud");
+            assert(count !== 0);
+            resolve();
+          }
+        );
+
+        // canceling the drainage.promise with an error triggers reject
+        drainage.cancel(new Error("fraud"));
+      }
+    );
+  }
+});
+
+test({
+  name: "canceling twice does no harm",
+  fn(): Promise<void> {
+    return new Promise(
+      (resolve: () => void, reject: (err: Error) => void): void => {
+        // a never-ending reader
+        const reader: Deno.Reader = {
+          async read(buf: Uint8Array): Promise<Deno.ReadResult> {
+            buf.fill(99);
+            return { nread: buf.length, eof: false };
+          }
+        };
+
+        let count: number = 0;
+
+        const drainage: Drainage = drain(
+          reader,
+          function ondata(chunk: Uint8Array): void {
+            ++count;
+          },
+          function onclose(): void {
+            assert(count !== 0);
+            resolve();
+          },
+          function onerror(err: Error): void {
+            reject(err);
+          }
+        );
+
+        // double-canceling the drainage.promise
+        drainage.cancel();
+        drainage.cancel();
+      }
+    );
+  }
+});
+
+test({
+  name: "can limit ondata handler invocations",
+  fn(): Promise<void> {
+    return new Promise(
+      (resolve: () => void, reject: (err: Error) => void): void => {
+        let read: number = 0;
+
+        // this reader pushes 10 chunks only
+        const reader: Deno.Reader = {
+          async read(buf: Uint8Array): Promise<Deno.ReadResult> {
+            if (read++ === 10) {
+              return { nread: 0, eof: true };
+            }
+
+            buf.fill(99);
+
+            return { nread: buf.length, eof: false };
+          }
+        };
+
+        let count: number = 0;
+
+        drain(
+          reader,
+          function ondata(chunk: Uint8Array): void {
+            ++count;
+          },
+          function onclose(): void {
+            assertEquals(count, 1);
+            resolve();
+          },
+          function onerror(err: Error): void {
+            reject(err);
+          },
+          { limit: 1 }
+        );
+      }
+    );
+  }
+});
+
+test({
+  name: "allows custom read timeouts",
+  fn(): Promise<void> {
+    return new Promise(
+      (resolve: () => void, reject: (err: Error) => void): void => {
+        // a slow reader
+        const reader: Deno.Reader = {
+          read(buf: Uint8Array): Promise<Deno.ReadResult> {
+            return new Promise((resolve: (result: Deno.ReadResult) => void): void => {
+              setTimeout((): void => {
+                buf.fill(99);
+                resolve({ nread: buf.length, eof: false })
+              }, 500)
+            })
+          }
+        };
+
+        drain(
+          reader,
+          function ondata(chunk: Uint8Array): void {},
+          function onclose(): void {
+            reject(null)
+          },
+          function onerror(err: Error): void {
+            assertEquals(err.message, "timeout")
+            resolve()
+          },
+          { maxReadTimeout: 400 }
+        );
+      }
+    );
   }
 });
 
